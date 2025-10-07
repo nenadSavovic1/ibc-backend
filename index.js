50dac839f1203f5c4fb107c4580c409be8e39329
Nenad Savovic <nenad_savovic@yahoo.com>
2025-10-07 12:22:06 +0200

    add random stuff

--- index.js
+++ index.js
@@ -124,3 +124,5 @@ app.post("/login", async (req, res) => {
 
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, () => console.log(`API running at http://localhost:${port}`));
+
+//ovo je komentar
